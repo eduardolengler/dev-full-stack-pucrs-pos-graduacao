aea07d0efa4f522a3be164836965bbd4eb3ce755
eduardolengler <eduardolengler@gmail.com>
2025-02-06 09:31:19 -0300

    Novos códigos da aula01 - Edson - POO.

---
+++ 03 - Programação Orientada A Objetos/Aula 01 - Edson Moreno/03c_const.js
@@ -0,0 +1,9 @@
+function testeConst() {
+    const const01 = {propriedade: "valor"};
+    console.log("01a." + const01.propriedade);
+
+    const01 = {propriedade: "novo valor"};
+    console.log("02a." + const01.propriedade);
+};
+
+testeConst();
